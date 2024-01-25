123ca08507d97574994932605bf678a05a2035ee
Cloud <cloud@Clouds-MacBook-Pro.local>
2024-01-25 05:46:01 +0300

    first commit

---
+++ api/routes/auth.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+
+
+
+
+
+  router.post("/login", async (req, res,next) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const passwordMatch = await bcrypt.compare(password, user.password)
+      if (!passwordMatch) {
+        return res.status(401).json({ message: 'Incorrect password' });
+      }
+  
+   
+      res.status(200).json(user)
+
+    } catch (error) {
+      next(error);
+    }
+  });
+module.exports = router;
